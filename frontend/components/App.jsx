cdf772bc9df35f989f6897eb8a900628d7257535
Garvin <gming27@gmail.com>
2019-10-10 11:32:09 -0400

    Adds Home page link

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,13 +3,14 @@ import GreetingContainer from "./Greeting/greeting_container";
 import LoginFormContainer from "./SessionForm/login_form_container";
 import SignupFormContainer from "./SessionForm/signup_form_container";
 import DemoLoginContainer from "./SessionForm/demo_login_container";
-import {Route} from "react-router-dom";
+import {Route, Link } from "react-router-dom";
 import {AuthRoute} from '../util/route_util';
 
 const App = () => (
 <div className="App">
-  <h1 className="primary">Coffee With Aliens
-  <GreetingContainer/>
+  <h1 className="primary">
+    <Link className="Home-link" to="/">Coffee With Aliens</Link>
+    <GreetingContainer/>
   </h1>
 
   
