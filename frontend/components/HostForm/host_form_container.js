e579b7d0d6f4c73e82e55be6d2830b7eb160786a
Garvin <gming27@gmail.com>
2019-10-11 00:54:59 -0400

    imports cleanErrors to HostFormContainer

--- frontend/components/HostForm/host_form_container.js
+++ frontend/components/HostForm/host_form_container.js
@@ -1,6 +1,7 @@
 import {connect} from 'react-redux';
 import { fetchUser, createHost, updateUser } from '../../actions/host_actions';
 import HostForm from './host_form';
+import { cleanErrors } from '../../actions/host_actions';
 
 const mapStateToProps = (state) => {
   let currentUserId = state && state.session && state.session.currentUser && state.session.currentUser.id
@@ -17,6 +18,7 @@ const mapDispatchToProps = dispatch =>{
 
   return({
     processHost: (host) => dispatch(createHost(host)),
+    cleanErrors: (errors) => dispatch(cleanErrors(errors))
   })
 };
 
