531663510f24556ec73a443f056290e6f4339530
Garvin <gming27@gmail.com>
2019-10-11 17:08:27 -0400

    removes coffee times ref from cofffee schedule third time is the charm

--- db/migrate/20191011210653_remove_ref_coffee_times_again.rb
+++ db/migrate/20191011210653_remove_ref_coffee_times_again.rb
@@ -1,5 +1,5 @@
 class RemoveRefCoffeeTimesAgain < ActiveRecord::Migration[5.2]
   def change
-    add_reference :coffee_schedules, :coffee_times, foreign_key: true
+    remove_reference :coffee_schedules, :coffee_times, foreign_key: true
   end
 end
