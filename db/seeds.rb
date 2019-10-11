7a3b222d6869fffa620658f3b9ce5ab88d0f4bac
Garvin <gming27@gmail.com>
2019-10-11 19:28:47 -0400

    Adds host and coffeetime seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,53 @@ user_two = User.new(
   star_system_id: star_one.id)
 
 user_two.save!
+
+user_three = User.new(
+  nickname: 'ET',
+  email: 'ET@brodo.com',
+  password: '12345678',
+  star_system_id: star_three.id)
+
+user_three.save!
+
+host_one = Host.create(
+  facebook: 'Nope'
+  intro: "Hi I'm Garvin"
+  story: "Born in Trinidad, raised in NYC."
+  topics: "Space travel, military strategy, philosophy"
+  user_id: user_one.id
+)
+
+host_two =  Host.create(
+  facebook: 'What is "facebook"?'
+  intro: 'ET is alien'
+  story: 'Glad to be home.  Crashed.  Tired.  Coffee good.'
+  topics: 'My trip far from home.  Also types of coffee.'
+  user_id: user_three.id 
+)
+
+coffee_one = CoffeeTime.create(
+  place: 'Earth'
+  date: Time.now    
+  start_time: 11.5
+  available_seats: 6
+  host_id: host_one.id
+  star_system_id: 1
+)
+coffee_two = CoffeeTime.create(
+  place: 'Earth'
+  date: Time.now    
+  start_time: 11
+  available_seats: 6
+  host_id: host_one.id
+  star_system_id: 1
+)
+
+coffee_three = CoffeeTime.create(
+  place: 'Brodo Asogi'
+  date: Time.now    
+  start_time: 8
+  available_seats: 6
+  host_id: host_two.id
+  star_system_id: 3
+)
