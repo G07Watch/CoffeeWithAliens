9a68603d30695d28f5fe68074235f9ee52bfa8fd
Garvin <gming27@gmail.com>
2019-10-10 23:32:34 -0400

    Creates clearErrors and cleanErrors actions in session_actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -3,6 +3,7 @@ import * as SessionApiUtil from '../util/session_api_util';
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER'
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER'
 export const RECEIVE_ERRORS = 'RECEIVE_ERRORS'
+export const CLEAN_ERRORS = 'CLEAN_ERRORS'
 
 export const signup = user => dispatch =>(
   SessionApiUtil.signup(user)
@@ -54,3 +55,14 @@ export const logoutCurrentUser = nullUser => ({
   nullUser
 })
 
+export const clearErrors = () => dispatch => (
+  ()=>dispatch(cleanErrors())
+)
+
+export const cleanErrors = ()=>{
+  let errors = null
+  return({
+    type: CLEAN_ERRORS,
+    errors
+  })
+}
