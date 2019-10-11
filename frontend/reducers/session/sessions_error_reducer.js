7964ef2aec6eff3571386b19015139162ae2c8cf
Garvin <gming27@gmail.com>
2019-10-11 00:42:22 -0400

    Updates sessionsErrorReducer to properly allow host errors to be displayed

--- frontend/reducers/session/sessions_error_reducer.js
+++ frontend/reducers/session/sessions_error_reducer.js
@@ -1,17 +1,20 @@
 import { RECEIVE_ERRORS, RECEIVE_CURRENT_USER, CLEAN_ERRORS } from '../../actions/session_actions';
-import {}
-
+import { RECEIVE_HOST_ERROR } from '../../actions/host_actions';
+ 
 const sessionErrorsReducer = (state = {}, action) => {
   Object.freeze(state); 
   switch(action.type){
     case RECEIVE_ERRORS:
       return action.errors;
 
-    case RECEIVE_CURRENT_USER:
-      return {};
+    case RECEIVE_HOST_ERROR:
+      return action.errors;
 
     case CLEAN_ERRORS:
       return action.errors;
+
+    case RECEIVE_CURRENT_USER:
+      return {};
 
     default:
       return state;
