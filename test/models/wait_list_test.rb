ad4cb6a44f72122ec028273f2b86689ceaf198a7
Garvin <gming27@gmail.com>
2019-10-11 21:18:06 -0400

    updates star system controller and host controller, updates indexes

--- test/models/wait_list_test.rb
+++ test/models/wait_list_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: wait_lists
+#
+#  id                 :integer          not null, primary key
+#  priority           :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  coffee_schedule_id :integer
+#
+
 require 'test_helper'
 
 class WaitListTest < ActiveSupport::TestCase
