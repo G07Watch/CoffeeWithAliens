f6fe809a79f6a4e74e33686a3892d0564cfe250e
Garvin <gming27@gmail.com>
2019-10-14 18:51:42 -0400

    refactors CoffeeSchedule to sort schedule by month

--- frontend/components/CoffeeSchedule/coffee_schedule.jsx
+++ frontend/components/CoffeeSchedule/coffee_schedule.jsx
@@ -16,12 +16,12 @@ class CoffeeSchedule extends React.Component{
   }
 
   render(){
-    let currentMonth = <p> {this.props.currentMonth} </p>;
+    let currentMonth;
     if (this.props.currentMonth){
       currentMonth = this.props.currentMonth;
     }
 
-    let nextMonth = <p>{this.props.nextMonth}</p>;
+    let nextMonth =;
     let coffeeMeets = 0 ;
     if (this.props.coffeeTimes){
       coffeeMeets = this.props.coffeeTimes;
@@ -38,93 +38,126 @@ class CoffeeSchedule extends React.Component{
     
     let starCoffeeSchedule=[];
     let coffeeTimesArr;
+    let currentMonthSchedule =<p></p>;
+    let nexMonthSchedule=<p></p>;
+    let currentMonthArr;
+    let nextMonthArr;
+
   
 
     if (coffeeMeets&& stars.length && 
       !(Object.entries(hosts).length === 0 && 
       hosts.constructor === Object)) {
 
-        for (const star of stars) {
-          // debugger
-          let coffeeTimes = coffeeMeets.filter( meeting => meeting.star_system_id === star.id )
-          
-          if (coffeeTimes.length !=0 && hosts){
-            // debugger
-         
-          coffeeTimesArr = coffeeTimes.map( meeting => {
-            // debugger
-           let hostId = meeting.host_id 
-           let hostName = hosts[hostId].name
 
-
-          return(
-          <Meeting
-            key={meeting.id}
-            meeting={meeting}
-            hostName={hostName}
-          />)
-        })
-
-        starCoffeeSchedule.push(
-          <li key={star.id}>
-            <h2 >
-              {star.name}
-            </h2>
-            <ul>
-              {coffeeTimesArr}
-            </ul>
-          </li>
-          )
+      for (meeting of coffeeMeets) {
+        if (meeeting.month === currentMonth) {
+          currentMonthArr.push(meeting);
         }
+
+        else nextMonthArr.push(meeting);
+      };
+
+      let count = 2;
+      let monthFlag = true;
+
+      while (count < 0){
+
+        let monthArr;
+        let month;
+
+        if (monthFlag === true) {
+          monthArr = currentMonthArr;
+          month = currentMonth
+        }
+
+        else {
+          monthArr = nextMonthArr;
+          month = nextMonth;
+        }
+
+
+          for (const star of stars) {
+            let coffeeTimes = monthArr.filter( meeting => meeting.star_system_id === star.id )
+            
+            if (coffeeTimes.length !=0 && hosts){
+          
+            coffeeTimesArr = coffeeTimes.map( meeting => {
+
+            let hostId = meeting.host_id 
+            let hostName = hosts[hostId].name
+
+
+            return(
+            <Meeting
+              key={meeting.id}
+              meeting={meeting}
+              hostName={hostName}
+            />)
+          })
+
+          starCoffeeSchedule.push(
+            <li key={star.id}>
+              <h2 >
+                {star.name}
+              </h2>
+              <ul>
+                {coffeeTimesArr}
+              </ul>
+            </li>
+            )
+          }
+        }
+
+        if (monthFlag === true){
+
+          currentMonthSchedule = <div className={month}>
+            <p>{month}Coffee Times</p>
+
+            <div className="jump">
+              Jump links
+            </div>
+
+            <div className="star">
+              <ul>
+                {starCoffeeSchedule}
+              </ul>
+            </div>
+
+          </div>
+        }
+
+        else{
+          nextMonthSchedule = <div className={month}>
+            <p>{month}Coffee Times</p>
+
+            <div className="jump">
+              Jump links
+            </div>
+
+            <div className="star">
+              <ul>
+                {starCoffeeSchedule}
+              </ul>
+            </div>
+
+          </div>
+        }
+
+
+
+        monthFlag = false;
+        count--;
       }
+
     }    
-
-
-    let currentMonthCoffeeSchedule;
-    let nexMonthCoffeeSchedule;
-
-
-    for (const starMeetings of starCoffeeSchedule){
-      if (currentMonth === starMeetings.coffeeTimesArr[0].month) {
-        currentMonthCoffeeSchedule = <div className={currentMonth}>
-          {starMeetings}
-        </div>
-      }
-    }
-
-
-
-
-
-
 
 
     return(
       <div>
-        {currentMonthCoffeeSchedule}
+        {currentMonthSchedule}
+        {nexMonthSchedule}
       </div>
-
-
-      // <div>
-      //   < div className = "month-display-current">
-
-      //     Month Display
-      //     {currentMonth}
-      //     {nextMonth}
-      //     <div className = "jump">
-      //       Jump
-      //     </div>
-
-      //     <div className = "star">
-      //       <ul>
-      //       {starCoffeeSchedule}
-      //       </ul>
-      //     </div>
-
-      //   </div>
-
-
-      // </div>
     )
   }
 
