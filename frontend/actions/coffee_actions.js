53ed7d00d5194bb809e3f25b387123f88eb6ceec
Garvin <gming27@gmail.com>
2019-10-16 12:53:02 -0400

    creates fetchCoffeeTime action for CoffeeTime Component

--- frontend/actions/coffee_actions.js
+++ frontend/actions/coffee_actions.js
@@ -1,6 +1,7 @@
 import * as CoffeeApiUtil from '../util/coffee_api_util';
 
 export const RECEIVE_COFFEE_TIMES = 'RECEIVE_COFFEE_TIMES';
+export const RECEIVE_COFFEE = 'RECEIVE_COFFEE';
 export const RECEIVE_COFFEE_ERRORS = 'RECEIVE_COFFEE_ERRORS';
 
 export const fetchCoffeeTimes = () => dispatch =>(
@@ -10,6 +11,21 @@ export const fetchCoffeeTimes = () => dispatch =>(
     errors => dispatch(receiveCoffeeErrors(errors))
   )
 );
+
+export const fetchCoffeeTime = id => dispatch =>(
+  CoffeeApiUtil.fetchCoffeeTime(id)
+  .then(
+    coffeeTime => dispatch(receiveCoffeeTime(coffeeTime)),
+    errors => dispatch(receiveCoffeeErrors(errors))
+  )
+) 
+
+export const receiveCoffeeTime =(coffeeTime) =>{
+  return({
+    type: RECEIVE_COFFEE,
+    coffeeTime
+  })
+}
 
 export const receiveCoffeeTimes = (coffeeTimes) =>{
   return({
