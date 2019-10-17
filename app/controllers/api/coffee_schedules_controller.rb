acf89edefb28b74b134402748c425fcd14dee3bf
Garvin <gming27@gmail.com>
2019-10-16 23:32:56 -0400

    properly names CoffeeSchedulesController

--- app/controllers/api/coffee_schedules_controller.rb
+++ app/controllers/api/coffee_schedules_controller.rb
@@ -1,15 +1,16 @@
-class Api::CoffeeTimesController < ApplicationController
+class Api::CoffeeSchedulesController < ApplicationController
 
   def create
   end
 
   def update
     @user = User.find_by(id: params[:user_id])
-
     @coffee_time = CoffeeTime.find_by(id: params[:coffee_time_id])
+
     @coffee_time.users << @user
+
     @coffee_signed= @user.coffee_times.find_by(id: params[:coffee_time_id])
-    
+
     if @coffee_signed
       @user_coffee_times = @user.coffee_times
       render :index
@@ -18,4 +19,7 @@ class Api::CoffeeTimesController < ApplicationController
     end
 
   end
+
+
+
 end
